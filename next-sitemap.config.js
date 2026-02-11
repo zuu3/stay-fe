4f36f9ebb10305447fdb8a9bec6f2619db2a103d
zuu3 <ojh27467242@gmail.com>
2026-02-11 17:34:32 +0900

    feat: Integrate next-sitemap for automatic sitemap and robots.txt generation.

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://stayrp.kro.kr",
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    changefreq: "daily",
+    priority: 0.7,
+};
